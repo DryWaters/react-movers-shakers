2d66af585e147231316e6c822738c89b590f60a7
Daniel Waters <danwater1@gmail.com>
2018-05-14 11:56:56 -0700

    Got react-google-maps component to work

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,22 +1,53 @@
-import React, { Component } from 'react';
+import React from "react"
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-class Map extends Component {
-  constructor(props) {
-    super(props);
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDdOc88zhd6KHz-i0UCxt8MJGFNWZRLpf0&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `500px`, width: `800px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) =>
+  <GoogleMap
+    defaultZoom={10}
+    defaultCenter={{ lat: 37.4800726, lng: -122.0811401 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} onClick={props.onMarkerClick} />}
+  </GoogleMap>
+)
 
+class MyFancyComponent extends React.PureComponent {
+  state = {
+    isMarkerShown: false,
   }
 
   componentDidMount() {
-    // fetch map
+    this.delayedShowMarker()
+  }
+
+  delayedShowMarker = () => {
+    setTimeout(() => {
+      this.setState({ isMarkerShown: true })
+    }, 3000)
+  }
+
+  handleMarkerClick = () => {
+    this.setState({ isMarkerShown: false })
+    this.delayedShowMarker()
   }
 
   render() {
     return (
-      <div>
-        <h1>Map</h1>
-      </div>
-    );
+      <MyMapComponent
+        isMarkerShown={this.state.isMarkerShown}
+        onMarkerClick={this.handleMarkerClick}
+      />
+    )
   }
 }
 
-export default Map;
+export default MyFancyComponent;
