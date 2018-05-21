1a45b74fe8cdee237762bd802ace18ff081f2c35
Daniel <danwater1@gmail.com>
2018-05-21 14:21:44 -0700

    Created styling for selected item

--- src/components/Earthquake/index.js
+++ src/components/Earthquake/index.js
@@ -3,12 +3,11 @@ import moment from 'moment';
 import PropTypes from 'prop-types';
 
 const Earthquake = ({ quake, toggleSelection, selection }) => (
-  <div onClick={() => toggleSelection(quake.id)} >
+  <div className={selection ? "quake-detail selected" : "quake-detail"} onClick={() => toggleSelection(quake.id)} >
     <div>Latitude: {quake.lat}  Longitude: {quake.lng}</div>
     <div>Date: {moment(quake.time).format('MM-DD-YYYY')}</div>
     <div>Magnitude: {quake.mag}</div>
     <div><a href={quake.url} target="#">More Information</a></div>
-    {selection && <div>Selected</div>}
   </div>
 );
 
