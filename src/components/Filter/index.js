d505b9e19fd058011dee8b2aac6f2672091fc248
Daniel <danwater1@gmail.com>
2018-05-21 14:10:34 -0700

    Updated styles for details

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Filter = props => (
-  <div>
+  <div className="filter">
     <input id="startDate" onChange={e => props.filterQuakes({ start: e.target.value, end: document.getElementById('endDate').value })} type="date" />
     <input id="endDate" onChange={e => props.filterQuakes({ start: document.getElementById('startDate').value, end: e.target.value })} type="date" />
   </div>
