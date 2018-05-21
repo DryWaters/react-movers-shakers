ccecbdd33ab8863c3a5b734c86945fed50cef98a
Daniel <danwater1@gmail.com>
2018-05-21 14:12:06 -0700

    Added proptypes to filter page

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Filter = props => (
   <div className="filter">
@@ -8,3 +9,7 @@ const Filter = props => (
 );
 
 export default Filter;
+
+Filter.propTypes = {
+  filterQuakes: PropTypes.func.isRequired,
+};
