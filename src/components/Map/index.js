76ef9afbedb60ebec3b3d3a47519ec60846ca068
Daniel Waters <danwater1@gmail.com>
2018-05-23 09:57:02 -0700

    Moved Google Maps loading to async HOC

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import moment from 'moment';
 import PropTypes from 'prop-types';
-import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
 
 const Map = (props) => {
   const defaultMapOptions = {
@@ -12,7 +12,7 @@ const Map = (props) => {
     disableAutoPan: true,
   };
 
-  const WrappedGoogleMap = withGoogleMap(() => (
+  const WrappedGoogleMap = withScriptjs(withGoogleMap(() => (
     <GoogleMap
       defaultCenter={{ lat: 37.4800726, lng: -122.211401 }}
       defaultZoom={window.screen.width > 400 ? 9 : 8}
@@ -36,10 +36,12 @@ const Map = (props) => {
         </Marker>
       ))}
     </GoogleMap>
-  ));
+  )));
 
   return (
     <WrappedGoogleMap
+      googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDdOc88zhd6KHz-i0UCxt8MJGFNWZRLpf0"
+      loadingElement={<div style={{ height: '100%' }} />}
       containerElement={<div id="map" style={{ height: '100vh', width: '100%' }} />}
       mapElement={<div style={{ height: '100%' }} />}
     />
