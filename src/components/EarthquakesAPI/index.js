7819c41bff1a76d6a5c73df31171f7a19a0fa80c
Daniel Waters <danwater1@gmail.com>
2018-05-23 10:04:44 -0700

    Chnaged USGS errors to alert message instead of silent

--- src/components/EarthquakesAPI/index.js
+++ src/components/EarthquakesAPI/index.js
@@ -4,7 +4,7 @@ const getAllQuakes = () =>
   fetch(`${api}&starttime=1900-01-01&latitude=37.4800726&longitude=-122.0811401&maxradiuskm=60&minmagnitude=5`)
     .then(res => res.json())
     .catch((error) => {
-      window.console.log(`Unable to contact the earthquake API with error: ${error}`);
+      window.alert(`Unable to contact the earthquake API with error: ${error}`); // eslint-disable-line
     });
 
 export default getAllQuakes;
